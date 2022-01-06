9b6a47f079210cf4a04d633984bb3776ca7000e8
Daniel Mai <daniel@danielmai.net>
2022-01-06 10:59:42 -0800

    Update i3 config.

--- bin/audio.go
+++ bin/audio.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -14,6 +16,16 @@ const (
 	LowUrgency      NotifyUrgency = "low"
 	NormalUrgency   NotifyUrgency = "normal"
 	CriticalUrgency NotifyUrgency = "critical"
+
+	// Sources
+	jabraSource = "alsa_input.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.mono-fallback"
+	yetiSource  = "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_LT_1906112229019B8B0391_111000-00.analog-stereo"
+
+	// Sinks
+	hdmiSink       = "alsa_output.pci-0000_81_00.1.hdmi-stereo"
+	jabraSink      = "alsa_output.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.analog-stereo"
+	headphonesSink = "alsa_output.usb-0d8c_C-Media_USB_Audio_Device-00.analog-stereo"
+	// headphonesSink = "alsa_output.usb-0d8c_C-Media_USB_Audio_Device-00.iec958-stereo"
 )
 
 func (n NotifyUrgency) String() string {
@@ -28,10 +40,15 @@ func (n NotifyUrgency) String() string {
 }
 
 func command(cmd string, args ...string) {
-	err := exec.Command(cmd, args...).Run()
+	var stdout, stderr bytes.Buffer
+	c := exec.Command(cmd, args...)
+	c.Stdout = &stdout
+	c.Stderr = &stderr
+	err := c.Run()
 	if err != nil {
-		notifyLevel(CriticalUrgency, "Command failed", 250*time.Millisecond)
-		log.Fatalf("%v", err)
+		errMsg := fmt.Sprintf("[%s]: %v %v", c.Args, err, stderr.String())
+		notifyLevel(CriticalUrgency, errMsg, 250*time.Millisecond)
+		log.Fatalf("%s\n", errMsg)
 	}
 }
 
@@ -46,13 +63,17 @@ func notifyLevel(level NotifyUrgency, msg string, dur time.Duration) {
 func switchDevice(s string) {
 	switch s {
 	case "headphones":
-		command("pactl", "set-default-sink", "alsa_output.usb-0d8c_C-Media_USB_Audio_Device-00.analog-stereo")
-		command("pactl", "set-default-source", "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_LT_1906112229019B8B0391_111000-00.analog-stereo")
+		command("pactl", "set-default-sink", headphonesSink)
+		command("pactl", "set-default-source", yetiSource)
 		notify("Set Output To Headphones", 250*time.Millisecond)
 	case "jabra":
-		command("pactl", "set-default-sink", "alsa_output.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.iec958-stereo")
-		command("pactl", "set-default-source", "alsa_input.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.multichannel-input")
+		command("pactl", "set-default-sink", jabraSink)
+		command("pactl", "set-default-source", jabraSource)
 		notify("Set Output To Jabra", 250*time.Millisecond)
+	case "hdmi":
+		command("pactl", "set-default-sink", hdmiSink)
+		command("pactl", "set-default-source", yetiSource)
+		notify("Set Output To HDMI", 250*time.Millisecond)
 	default:
 		log.Fatalf("Unknown device: %v", s)
 	}
@@ -63,6 +84,8 @@ func isDevice(s string) bool {
 	case "headphones":
 		return true
 	case "jabra":
+		return true
+	case "hdmi":
 		return true
 	default:
 		return false
@@ -83,13 +106,15 @@ func isMuteCommand(s string) bool {
 func toggleMute(s string) {
 	switch s {
 	case "mute-mic":
-		command("pactl", "set-source-mute", "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_LT_1906112229019B8B0391_111000-00.analog-stereo", "1")
-		command("pactl", "set-source-mute", "alsa_input.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.multichannel-input", "1")
-		notify("Muted", 250*time.Millisecond)
+		command("pactl", "set-source-mute", yetiSource, "1")
+		// command("pactl", "set-source-mute", jabraSource, "1")
+		// notify("Muted", 250*time.Millisecond)
+		// command("i3-msg", "bar", "mode", "dock", "mute-bar")
 	case "unmute-mic":
-		command("pactl", "set-source-mute", "alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_LT_1906112229019B8B0391_111000-00.analog-stereo", "0")
-		command("pactl", "set-source-mute", "alsa_input.usb-0b0e_Jabra_Speak_710_70BF9200C6A0-00.multichannel-input", "0")
-		notify("Unmuted", 250*time.Millisecond)
+		command("pactl", "set-source-mute", yetiSource, "0")
+		// command("pactl", "set-source-mute", jabraSource, "0")
+		// notify("Unmuted", 250*time.Millisecond)
+		// command("i3-msg", "bar", "mode", "invisible", "mute-bar")
 	default:
 		log.Fatalf("Unknown command: %v", s)
 	}
